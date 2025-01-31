2f110f5b541fd071f4e5ed38805291732932d3d5
Mr_Dwj <explorer-dong@foxmail.com>
2025-01-31 15:57:55 +0800

    feat: 百度统计

---
+++ docs/injects/javascripts/baidu_tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?2671a19c27555d4fe9a537eb8b224efc";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();

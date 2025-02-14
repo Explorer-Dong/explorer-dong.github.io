6ea0e2a52bf8510cc90d4153acca7c5aae8a92c9
Mr_Dwj <explorer-dong@foxmail.com>
2025-02-14 21:42:43 +0800

    upd: search file url

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,19 @@ echo "部署到 Aliyun Server..."
 # 构建
 mkdocs build
 
+# 在 OSS 中同步 search_index.json
+ossutil cp e:/_notes/Wiki/site/search/search_index.json oss://dwj-oss/search-files/ --force
+
+# 修改 js 中的 search url
+OLD_STRING="search/search_index.json"
+NEW_STRING="https://dwj-oss.oss-cn-nanjing.aliyuncs.com/search-files/search_index.json"
+for file in site/assets/javascripts/bundle.*.min.js; do
+    if [[ -f "$file" ]]; then
+        sed -i "s|$OLD_STRING|$NEW_STRING|g" "$file"
+        echo "已替换文件: $file"
+    fi
+done
+
 # 压缩传输
 tar -czf site.tar.gz site/
 sftp git@47.100.217.241 << EOF

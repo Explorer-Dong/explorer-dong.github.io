945276515e216253b45ed35d3c2dd8492b316764
Mr_Dwj <explorer-dong@foxmail.com>
2025-04-27 17:29:33 +0800

    upd: Microsoft Webmasters Tools

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ git push
 echo "部署到 Aliyun OSS..."
 mkdocs build
 ossutil rm -r oss://wiki-website/ -f
+ossutil cp oss://dwj-oss/cdn-files/BingSiteAuth.xml oss://wiki-website/
 ossutil cp -r ./site/ oss://wiki-website/

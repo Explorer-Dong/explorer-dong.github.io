39ba06ba17409a0054ed0fa89e05fdb705470cbd
Mr_Dwj <explorer-dong@foxmail.com>
2025-05-23 23:07:41 +0800

    rm: github pages deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
-echo "部署到 GitHub Pages..."
-git push
-
-echo "部署到 Aliyun OSS..."
 mkdocs build
 ossutil rm -r oss://wiki-website/ -f
 ossutil cp oss://dwj-oss/cdn-files/BingSiteAuth.xml oss://wiki-website/

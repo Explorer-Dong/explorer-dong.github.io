1b33f6b0a052af45fe744fdb3d260360a09bf2e4
Mr_Dwj <explorer-dong@foxmail.com>
2025-06-04 08:26:51 +0800

    fix: oss path

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ git push
 
 mkdocs build
 ossutil rm -r oss://wiki-website/ -f
-ossutil cp oss://dwj-oss/cdn-files/BingSiteAuth.xml oss://wiki-website/
+ossutil cp oss://dwj-oss/files/BingSiteAuth.xml oss://wiki-website/
 ossutil cp -r ./site/ oss://wiki-website/
